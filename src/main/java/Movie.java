d23328e599425b8c1e112ae397e7d76da5bd7f1e
fgiaquintaruiz <fgiaquintaruiz@gmail.com>
2021-02-08 12:18:54 -0300

    Video part 2 Domate movie tests added

---
+++ src/main/java/Movie.java
@@ -0,0 +1,12 @@
+public class Movie {
+
+    private int copies;
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void addCopy() {
+        copies++;
+    }
+}
--- src/test/java/DonateMovieTest.java
+++ src/test/java/DonateMovieTest.java
@@ -1,10 +1,26 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DonateMovieTest {
 
-    @Test
-    public void donateMovie(){
+    Library library;
+    Movie movie;
 
+    public DonateMovieTest(){
+        library = new Library();
+        movie = new Movie();
+    }
+
+    @Test
+    public void movieAddedToCatalogue(){
+        library.donate(movie);
+        Assertions.assertTrue(library.contains(movie));
+    }
+
+    @Test
+    public void rentalCopyAdded(){
+        library.donate(movie);
+        Assertions.assertEquals(1, movie.getCopies());
     }
 
 }
